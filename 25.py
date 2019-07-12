0500f24000bec8ed9cadcfc7b053e8d250ed6794
santhoshbabu4546 <51222658+santhoshbabu4546@users.noreply.github.com>
2019-07-12 17:05:07 +0530

    Create 25.py

---
+++ 25.py
@@ -0,0 +1,4 @@
+int = input()
+list = ["X" , "I" , "II" , "III" , "IV" , "V" , "VI" , "VII" , "VIII" , "IX" , "X" , "XI" , "XII" , "XIII" , "XIV" , "XV" , "XVI" , "XVII" , "XVIII" , "XIX", "XX"]
+if int in list:
+  print(list.index(int))
