67cc23a209540e305cf4cc7f5c4172f01774be90
santhoshbabu4546 <51222658+santhoshbabu4546@users.noreply.github.com>
2019-07-17 13:37:17 +0530

    Create 51.py

---
+++ 51.py
@@ -0,0 +1,13 @@
+b=int(input())
+g=2
+h=1
+while(gb):
+       if(b%g==0):
+              h=0
+              break
+       else:
+              g=g+1
+if h==0:
+       print('no')
+else:
+       print('yes')
