08bee3df6876ea452899efd17acc793cf8ddf89d
santhoshbabu4546 <51222658+santhoshbabu4546@users.noreply.github.com>
2019-08-01 16:36:25 +0530

    Create player.set-7-61.py

---
+++ player.set-7-61.py
@@ -0,0 +1,11 @@
+x,y1=list(map(int,input().split()))
+u=list(map(int,input().split()))
+z=0
+for k in range(x-1):
+  for p in range(1,x):
+    if u[k]+u[p]==y1:
+      z=1
+if z==0:
+  print("no")
+else:
+  print("yes")
